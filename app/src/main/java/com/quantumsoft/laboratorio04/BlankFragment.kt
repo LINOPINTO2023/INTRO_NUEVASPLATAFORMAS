e0ce7bac0b82a05513575a9424667ba88a2af1cd
Frank C <84058890+Frank-C0@users.noreply.github.com>
2024-05-22 20:06:19 -0500

    Si aporté a este trabajo.

--- app/src/main/java/com/quantumsoft/laboratorio04/BlankFragment.kt
+++ app/src/main/java/com/quantumsoft/laboratorio04/BlankFragment.kt
@@ -8,11 +8,7 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BlankFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class BlankFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var mParam1: String? = null
@@ -44,14 +40,6 @@ class BlankFragment : Fragment() {
         //private const val ARG_PARAM1 = "param1"
         //private const val ARG_PARAM2 = "param2"
 
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragment.
-         */
         // TODO: Rename and change types and number of parameters
 
         /**fun newInstance(param1: String?, param2: String?): BlankFragment {
