03b086a43cba2127065e3d023f1970f95370d6fa
MendozaEmerson <emendozahi@unsa.edu.pe>
2024-05-22 22:59:43 -0500

    Se hicieron instancias tanto en MainActivity como en su Fragment.

--- app/src/main/java/com/example/fragments/FragmentMessage.kt
+++ app/src/main/java/com/example/fragments/FragmentMessage.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputEditText
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,26 +37,40 @@ class FragmentMessage : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false)
+        //return inflater.inflate(R.layout.fragment_message, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_message, container, false)
+
+        val btnSendMessage = view.findViewById<Button>(R.id.btnMessage)
+        val edtMessage = view.findViewById<EditText>(R.id.edtMessage)
+
+        // Cuando se presiona el boton obtiene el mensaje como String y se lo
+        // pasa a la funcion lambda
+        btnSendMessage.setOnClickListener {
+            val message = edtMessage.text.toString()
+            clickBtnSendMessage(message)
+        }
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentMessage.
-         */
-        // TODO: Rename and change types and number of parameters
+        private lateinit var clickBtnSendMessage: (String) -> Unit
+
+        // Toma como parametro una funcion lambda para instanciar la del MainActivity
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentMessage().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance(clickBtnSendMessage: (String) -> Unit): FragmentMessage {
+            val blankFragment = FragmentMessage()
+            this.clickBtnSendMessage = clickBtnSendMessage
+
+            return blankFragment
+        }
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            FragmentMessage().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
     }
 }
--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
 class MainActivity : AppCompatActivity() {
 
-    private  lateinit var txtMensaje:TextView;
+    private lateinit var txtMensaje: TextView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,9 +23,18 @@ class MainActivity : AppCompatActivity() {
         }
 
         txtMensaje = findViewById<TextView>(R.id.txtMessage)
+
+        // Se instancia el fragment con parametro a la funcion lambda del Activity
+        val blankFragment = FragmentMessage.newInstance(response)
+
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace(R.id.frameLayout, blankFragment)
+        }
     }
 
-    val response:(String)->Unit={mensaje ->
-        txtMensaje.text=mensaje;
+    // Crea una funcion lambda que sera parametro del fragment
+    val response: (String) -> Unit = { mensaje ->
+        txtMensaje.text = mensaje;
     }
 }
