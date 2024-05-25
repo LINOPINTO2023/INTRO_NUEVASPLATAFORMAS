bee550419c39dffc49c91f92e36658c25931a6a8
Cristian-hash <cpacori@unsa.edu.pe>
2024-05-25 18:03:17 -0500

    Lab4 FragmentLambda: Correciones de la creacion del proyecto

--- FragmentLambda/app/src/main/java/com/example/fragmentlambda/MainActivity.kt
+++ FragmentLambda/app/src/main/java/com/example/fragmentlambda/MainActivity.kt
@@ -1,16 +1,15 @@
-package com.example.lab4
+package com.example.fragmentlambda
 
-import android.os.Build
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
