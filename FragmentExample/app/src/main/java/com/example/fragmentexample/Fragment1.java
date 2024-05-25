68d9ed9809742e0ac5742c7a00e6c9e95be7a902
Cristian-hash <cpacori@unsa.edu.pe>
2024-05-24 21:46:50 -0500

    Fragments: edicion fragments.xml

--- FragmentExample/app/src/main/java/com/example/fragmentexample/Fragment1.java
+++ FragmentExample/app/src/main/java/com/example/fragmentexample/Fragment1.java
@@ -15,7 +15,7 @@ import android.view.ViewGroup;
  */
 public class Fragment1 extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -28,15 +28,6 @@ public class Fragment1 extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment1.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Fragment1 newInstance(String param1, String param2) {
         Fragment1 fragment = new Fragment1();
         Bundle args = new Bundle();
