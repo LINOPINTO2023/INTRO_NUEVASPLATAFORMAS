c6aa41028ae53f6ac62c6fd68a1c8bd13658ec07
MendozaEmerson <emendozahi@unsa.edu.pe>
2024-05-25 19:23:54 -0500

    Se creo un layout para el menu, se editaron los layouts de MainActivity y el fragment para el registro.

--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -8,10 +8,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
+import com.example.fragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var txtMensaje: TextView;
+    private  lateinit var binding: ActivityMainBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,6 +24,17 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+       // supportFragmentManager.beginTransaction().add(R.id.frameLayout, )
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.mRegister -> supportFragmentManager.beginTransaction().replace(R.id.frameLayout, RegisterFragment()).commit()
+            }
+            true
+        }
         //txtMensaje = findViewById<TextView>(R.id.txtMessage)
 
         // Se instancia el fragment con parametro a la funcion lambda del Activity
